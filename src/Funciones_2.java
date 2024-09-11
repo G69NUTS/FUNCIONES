4b2cbdc3a61f69bac6ee952ba2359dd841dbebae
G69NUTS <emontealegrev@udistrital.edu.co>
2024-09-11 11:46:04 -0500

    Funciones2

--- src/Funciones_2.java
+++ src/Funciones_2.java
@@ -4,111 +4,149 @@ import java.util.Scanner;
 public class Funciones_2{
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
+        int Salida = 0;
+        do {
+            //MENU
+            System.out.println("Muy buenas DIAS/TARDES que desea hacer:");
+            MENU();
+        }while( Salida!=0);
+            System.out.println("Digita tu opcion: ");
+            int Respuesta = leer.nextInt();
 
-        //MENU
-
-        System.out.println("Muy buenas DIAS/TARDES que desea hacer:");
-        MENU();
-        System.out.println("Digita tu opcion: ");
-        int Respuesta = leer.nextInt();
-
-        //SUMA
-        if(Respuesta==1) {
-            System.out.println("Buenos dias dijite porfavor los numeros: ");
-            System.out.println("Numero 1:");
-            int num_1 = leer.nextInt();
-            System.out.println("Numero 2:");
-            int num_2 = leer.nextInt();
-            System.out.println("La suma de los numeros es: " + Suma(num_1, num_2));
-        }
-        //MULTIPLICACION
-        if(Respuesta==2) {
-            System.out.println("Dijite dos numeros para multiplicar");
-            System.out.println("Numero 1");
-            int Mul = leer.nextInt();
-            System.out.println("Numero 2");
-            int Por = leer.nextInt();
-            int Res = 0;
-            System.out.println("La multiplicacion de: " + Mul + " por: " + Por + " Es: " + Multiplicacion(Mul, Por, Res));
-        }
-        //POTENCIACION
-        if(Respuesta==3) {
-            System.out.println("Dijite La Base de la potenciacion:");
-            System.out.println("Base: ");
-            int Base = leer.nextInt();
-            System.out.println("Potencia: ");
-            int Potencia = leer.nextInt();
-            System.out.println("El resultado de : " + Base + " a la potencia de " + Potencia + " Es igual a: " + Potenciacion(Base, Potencia, Base));
-        }
-        //FACTORIAL
-        if(Respuesta==4) {
-            System.out.println("Dijite un un numero para sacarle factorial");
-            System.out.println("Numero: ");
-            int Num = leer.nextInt();
-            int Sex = 1;
-            int Fac = Num - 1;
-            System.out.println("El factorial de: " + Num + " Es: " + Factorial(Num, Sex, Fac));
-        }
-        //AREA DE UN CUADRADO
-        if(Respuesta==5) {
-            System.out.println("Dijite el Lado de un cuadrado para sacar si area");
-            System.out.println("LADO:");
-            int Lado = leer.nextInt();
-            int area = 0;
-            System.out.println("El area del cuadrado es: " + Area(Lado, area));
-        }
-        //AREA DE UN CIRCULO
-        if(Respuesta==6) {
-            System.out.println("De el Radio de un circulo para calcular su AREA");
-            System.out.println("RADIO");
-            double Rad = leer.nextInt();
-            double AREAR = 0;
-            System.out.println("El area del circulo es: " + AREA_2(Rad, AREAR));
-        }
-        //VOLUMEN DE UN CONO
-        if(Respuesta==7) {
-            System.out.println("Dijite la altura y le radio de un Cono para Calcular su VOLUMEN");
-            System.out.println("RADIO:");
-            double Tad = leer.nextDouble();
-            System.out.println("ALTURA:");
-            int h = leer.nextInt();
-            double NANI = 0;
-            int DIV = 3;
-            System.out.println("El Volumen de el Cono es: " + areacono(Tad, h, NANI, DIV));
-        }
-        //SERIE DE FIBONACCI
-        if(Respuesta==8) {
-            System.out.println("Dijite El numero al cual quiere hacer la serie de fibonacci");
-            System.out.println("NUMERO:");
-            int n = leer.nextInt();
-            System.out.println("Los primeros " + n + " números de la serie de Fibonacci son:");
-            mostrarSerieFibonacci(n);
-        }
-        //ARREGLOS SUMAS
-
-        if(Respuesta==9){
-            System.out.println("Digite la cantidad de numeros que quiere sumar");
-            System.out.println("Numeros:");
-            int Tamano = leer.nextInt();
-            int Total =0;
-            int [] arreglo = new int[Tamano];
-            for(int i=0;i<arreglo.length;i++){
-                System.out.println("Digite los numeros");
-                arreglo [i] = leer.nextInt();
+            //SUMA
+            if (Respuesta == 1) {
+                System.out.println("Buenos dias dijite porfavor los numeros: ");
+                System.out.println("Numero 1:");
+                int num_1 = leer.nextInt();
+                System.out.println("Numero 2:");
+                int num_2 = leer.nextInt();
+                System.out.println("La suma de los numeros es: " + Suma(num_1, num_2));
             }
-            System.out.println(sumaArreglos(arreglo,Total));
-        }
-        //ARREGLOS MENOR MAYOR
+            //MULTIPLICACION
+            if (Respuesta == 2) {
+                System.out.println("Dijite dos numeros para multiplicar");
+                System.out.println("Numero 1");
+                int Mul = leer.nextInt();
+                System.out.println("Numero 2");
+                int Por = leer.nextInt();
+                int Res = 0;
+                System.out.println("La multiplicacion de: " + Mul + " por: " + Por + " Es: " + Multiplicacion(Mul, Por, Res));
+            }
+            //POTENCIACION
+            if (Respuesta == 3) {
+                System.out.println("Dijite La Base de la potenciacion:");
+                System.out.println("Base: ");
+                int Base = leer.nextInt();
+                System.out.println("Potencia: ");
+                int Potencia = leer.nextInt();
+                System.out.println("El resultado de : " + Base + " a la potencia de " + Potencia + " Es igual a: " + Potenciacion(Base, Potencia, Base));
+            }
+            //FACTORIAL
+            if (Respuesta == 4) {
+                System.out.println("Dijite un un numero para sacarle factorial");
+                System.out.println("Numero: ");
+                int Num = leer.nextInt();
+                int Sex = 1;
+                int Fac = Num - 1;
+                System.out.println("El factorial de: " + Num + " Es: " + Factorial(Num, Sex, Fac));
+            }
+            //AREA DE UN CUADRADO
+            if (Respuesta == 5) {
+                System.out.println("Dijite el Lado de un cuadrado para sacar si area");
+                System.out.println("LADO:");
+                int Lado = leer.nextInt();
+                int area = 0;
+                System.out.println("El area del cuadrado es: " + Area(Lado, area));
+            }
+            //AREA DE UN CIRCULO
+            if (Respuesta == 6) {
+                System.out.println("De el Radio de un circulo para calcular su AREA");
+                System.out.println("RADIO");
+                double Rad = leer.nextInt();
+                double AREAR = 0;
+                System.out.println("El area del circulo es: " + AREA_2(Rad, AREAR));
+            }
+            //VOLUMEN DE UN CONO
+            if (Respuesta == 7) {
+                System.out.println("Dijite la altura y le radio de un Cono para Calcular su VOLUMEN");
+                System.out.println("RADIO:");
+                double Tad = leer.nextDouble();
+                System.out.println("ALTURA:");
+                int h = leer.nextInt();
+                double NANI = 0;
+                int DIV = 3;
+                System.out.println("El Volumen de el Cono es: " + areacono(Tad, h, NANI, DIV));
+            }
+            //SERIE DE FIBONACCI
+            if (Respuesta == 8) {
+                System.out.println("Dijite El numero al cual quiere hacer la serie de fibonacci");
+                System.out.println("NUMERO:");
+                int n = leer.nextInt();
+                System.out.println("Los primeros " + n + " números de la serie de Fibonacci son:");
+                mostrarSerieFibonacci(n);
+            }
+            //ARREGLOS SUMAS
 
-        if(Respuesta==10){
-            System.out.println("Dijite la cantidad de numeros que quiere comparar");
+            if (Respuesta == 9) {
+                System.out.println("Digite la cantidad de numeros que quiere sumar");
+                System.out.println("Numeros:");
+                int Tamano = leer.nextInt();
+                int Total = 0;
+                int[] arreglo = new int[Tamano];
+                for (int i = 0; i < arreglo.length; i++) {
+                    System.out.println("Digite los numeros");
+                    arreglo[i] = leer.nextInt();
+                }
+                System.out.println(sumaArreglos(arreglo, Total));
+            }
+            //ARREGLOS MENOR MAYOR
 
-        }
+            if (Respuesta == 10) {
+                System.out.println("Dijite los Diez numeros que quiere comparar");
+                int[] Numeros = new int[10];
+                for(int i = 0; i < 10;i++) {
+                    System.out.println("Numero: ");
+                    Numeros[i]= leer.nextInt();
+                }
+
+            }
+            // MULTIPLICACION MATRICES
+
+            if(Respuesta == 11){
+                System.out.println("Ingrese porfavor los numeros que quiere multiplicar");
+                int[][] Matriz1 = new int[4][4];
+                int[][] Matriz2 = new int[4][4];
+                for(int i = 0; i < 8;i++) {
+                    System.out.println("Numero: ");
+                    Matriz1[i][i]= leer.nextInt();
+                }
+                for(int i = 0; i < 8;i++) {
+                    System.out.println("Numero: ");
+                    Matriz2[i][i]= leer.nextInt();
+                }
+
+
+            }
 
 
 
 
 
+
+
+
+
+
+
+
+
+            //SALIDA DEL MENU
+
+            if(Respuesta == 0){
+                int Wow=0;
+                System.out.println("Gracias por utilizar Nuestro programa");
+                salidaMenu(Salida,Wow);
+            }
 
 
 
@@ -191,7 +229,8 @@ public class Funciones_2{
 
     public static void MENU() {
         System.out.println("""
-                 1. Sumar 2 numeros
+                    0. Terminar Programa
+                    1. Sumar 2 numeros
                     2. Multiplicar 2 numeros con las sumas del 1
                     3. Potenciar 2 numeros con las sumas del 1
                     4. Calcular el factorial de 1 numero
@@ -206,18 +245,96 @@ public class Funciones_2{
     //FUNCION SUMA ARREGLOS
 
     public static int sumaArreglos(int[] arreglo,int Total){
-
-        for (int i=0;i<arreglo.length;i++){
-
-            Total += arreglo[i];
-
+        for (int j : arreglo) {
+            Total += j;
         }
-
         return Total;
     }
 
+    //FUNCION SALIDA DEL MENU
 
+    public static int salidaMenu(int Salida,int Wow){
+        Wow = Salida++;
+      return Wow ;
+    }
 
+    //FUNCION COMPARACION ARREGLO
+
+    public static int comparacionArreglo(int[]Numeros){
+
+        if(Numeros[0]<Numeros[1]){
+
+        }
+        if(Numeros[1]<Numeros[2]){
+
+        }
+        if(Numeros[2]<Numeros[3]){
+
+        }
+        if(Numeros[3]<Numeros[4]){
+
+        }
+        if(Numeros[4]<Numeros[5]){
+
+        }
+        if(Numeros[5]<Numeros[6]){
+
+        }
+        if(Numeros[6]<Numeros[7]){
+
+        }
+        if(Numeros[7]<Numeros[8]){
+
+        }
+        if(Numeros[8]<Numeros[9]){
+
+        }
+        if(Numeros[9]<Numeros[10]){
+
+        }
+        if(Numeros[10]<Numeros[0]) {
+
+        }
+        if(Numeros[1]>Numeros[0]) {
+
+        }
+        if(Numeros[2]>Numeros[1]) {
+
+        }
+        if(Numeros[3]>Numeros[2]) {
+
+        }
+        if(Numeros[4]>Numeros[3]) {
+
+        }
+        if(Numeros[5]>Numeros[6]) {
+
+        }
+        if(Numeros[6]>Numeros[5]) {
+
+        }
+        if(Numeros[7]>Numeros[8]) {
+
+        }
+        if(Numeros[8]>Numeros[9]) {
+
+        }
+        if(Numeros[9]>Numeros[10]) {
+
+        }
+        if(Numeros[10]>Numeros[0]) {
+
+        }
+
+        return Numeros[0];
+    }
+
+    //FUNCION MULTIPLICACION DE MATRICES
+
+    public static int multiplicacionMatrices(int[][]Matriz1){
+
+    return Matriz1[2][3];
+    }
 
         //Principios SOLID
         //NOTA IMPORTANTE PARA UNAS SEMANAS: [I@85ede7b
